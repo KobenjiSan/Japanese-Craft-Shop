88eb0a831a214b4a1488caee83f0a71c3616fc5b
Sam <samuelgkeller@gmail.com>
2025-07-31 21:38:49 -0400

    Added more styling, will need to refactor later

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,10 +6,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#4A3426',
+        primary: '#3E2E24',
+        'primary-dark': '#2E231D',
+        'primary-light': '#5D473A',
         secondary: '#D8C3A5',
+        'secondary-dark': '#AE9A7F',
+        'secondary-light': '#EEDDC5',
         'brand-dark': '#1C1B1A',
         'brand-light': '#F5F1EB',
+      },
+      fontFamily: {
+        japanese: ['"Sawarabi Mincho"', 'serif'],
       },
     },
   },

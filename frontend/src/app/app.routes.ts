9fc76ef4a671bf454dda9e0c970f3ac4dbb84a0f
Sam <samuelgkeller@gmail.com>
2025-08-03 10:58:20 -0400

    refactor: Refactored to utilize layouts. Narrowed Hero section and added minimal content to footer.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,18 +1,25 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () =>
-            import('./pages/home/home.component').then(
-                (m) => m.HomeComponent
-            )
+        component: MainLayoutComponent,
+        children: [
+            {path: '', component: HomeComponent},
+            {path: 'shop', component: ShopComponent},
+            // TODO : add about component
+        ]
     },
     {
-        path: 'shop',
-        loadComponent: () => 
-            import('./pages/shop/shop.component').then(
-                (m) => m.ShopComponent
-            )
+        path: '',
+        component: AuthLayoutComponent,
+        children: [
+            // TODO : add login component
+            // TODO : add register component
+        ]
     }
 ];

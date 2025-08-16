8856b22e6fd70909070dfad5b902f4e6eb505cb6
Sam <samuelgkeller@gmail.com>
2025-08-16 18:18:59 -0400

    feat: Added button to user profile to allow users to return to shop home

--- frontend/src/app/features/users/user-profile/user-profile.component.ts
+++ frontend/src/app/features/users/user-profile/user-profile.component.ts
@@ -4,7 +4,9 @@ import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
@@ -19,7 +21,6 @@ export class UserProfileComponent {
     this.router.navigate(['/']);
   }
 
-  // NOT sure why routerLink doesnt work so this is what we are using
   toDashboard(){
     this.router.navigate(['/admin']); 
   }

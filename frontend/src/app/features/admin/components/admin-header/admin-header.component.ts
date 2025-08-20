ce9a4c95175254831e2d3941981c5e03f004622a
Sam <samuelgkeller@gmail.com>
2025-08-19 23:48:09 -0400

    feat: Admin now has button to link straight to dashboard and no longer has buttons to user-profile and favorites-page in header. Admin logs out through dashboard

--- frontend/src/app/features/admin/components/admin-header/admin-header.component.ts
+++ frontend/src/app/features/admin/components/admin-header/admin-header.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-admin-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './admin-header.component.html',
   styleUrl: './admin-header.component.scss'
 })
 export class AdminHeaderComponent {
+   auth = inject(AuthService);
+  router = inject(Router);
 
+  onLogout(){
+    this.auth.logout();
+    this.router.navigate(['/']);
+  }
 }
